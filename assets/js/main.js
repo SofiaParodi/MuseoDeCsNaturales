641885d9a1450d9321c24d64c9e2649726f2136e
SofiaParodi <sofia.parodi503@gmail.com>
2024-02-14 18:12:42 -0300

    feat: sección contact y sus versiones responsive.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// b u r g u e r  m e n u
 const nav = document.querySelector("#header__nav");
 const openBtn = document.querySelector("#burger-menu__open");
 const closeBtn = document.querySelector("#burger-menu__close")
@@ -10,3 +11,13 @@ closeBtn.addEventListener("click", () => {
     nav.classList.remove("header__nav-visible");
     
 })
+
+// f o r m
+const successMessage = document.querySelector("#success__message");
+const form = document.querySelector("#success__form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    successMessage.classList.add("section__success-visible");
+    form.classList.add("section__form-invisible");
+});
